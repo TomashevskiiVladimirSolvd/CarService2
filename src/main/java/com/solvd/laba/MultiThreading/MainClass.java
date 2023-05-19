ff542c0d51e1e45293c679ea9baccab7a753246a
TomashevskiiVladimir <vladimir.tomashevskii@tekschool.us>
2023-05-19 15:53:22 -0400

    new16

--- src/main/java/com/solvd/laba/MultiThreading/MainClass.java
+++ src/main/java/com/solvd/laba/MultiThreading/MainClass.java
@@ -10,43 +10,52 @@ public class MainClass {
         ExecutorService executor = Executors.newFixedThreadPool(7);
 
         for (int i = 0; i < 5; i++) {
-            executor.submit(() -> {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    CompletableFuture<BufferedWriter> connectionFuture = connectionPool.getConnection();
+                    try {
+                        BufferedWriter connection = connectionFuture.get();
+                        connection.write("This is a first line.");
+                        connection.newLine();
+                        connection.flush();
+                        connectionPool.releaseConnection(connection);
+                    } catch (InterruptedException | ExecutionException | IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
                 CompletableFuture<BufferedWriter> connectionFuture = connectionPool.getConnection();
                 try {
                     BufferedWriter connection = connectionFuture.get();
-                    connection.write("This is First line of text");
+                    connection.write("This is a second line.");
                     connection.newLine();
                     connection.flush();
                     connectionPool.releaseConnection(connection);
                 } catch (InterruptedException | ExecutionException | IOException e) {
                     e.printStackTrace();
                 }
-            });
-        }
-
-        executor.submit(() -> {
-            CompletableFuture<BufferedWriter> connectionFuture = connectionPool.getConnection();
-            try {
-                BufferedWriter connection = connectionFuture.get();
-                connection.write("This is a second line.");
-                connection.newLine();
-                connection.flush();
-                connectionPool.releaseConnection(connection);
-            } catch (InterruptedException | ExecutionException | IOException e) {
-                e.printStackTrace();
             }
         });
 
-        executor.submit(() -> {
-            CompletableFuture<BufferedWriter> connectionFuture = connectionPool.getConnection();
-            try {
-                BufferedWriter connection = connectionFuture.get();
-                connection.write("This is a third line.");
-                connection.newLine();
-                connection.flush();
-                connectionPool.releaseConnection(connection);
-            } catch (InterruptedException | ExecutionException | IOException e) {
-                e.printStackTrace();
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                CompletableFuture<BufferedWriter> connectionFuture = connectionPool.getConnection();
+                try {
+                    BufferedWriter connection = connectionFuture.get();
+                    connection.write("This is a third line.");
+                    connection.newLine();
+                    connection.flush();
+                    connectionPool.releaseConnection(connection);
+                } catch (InterruptedException | ExecutionException | IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
